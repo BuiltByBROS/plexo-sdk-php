3e0ee75ea468c2dd495065242dc30144dbb145b6
Pablo Bangueses <pablo.bangueses@bros.me>
2017-08-06 09:49:06 -0300

    Exceptions.

--- src/Exception/SignatureException.php
+++ src/Exception/SignatureException.php
@@ -3,7 +3,7 @@ namespace Plexo\Sdk\Exception;
 
 class SignatureException extends PlexoException {
     
-    public function __construct($message) {
-        parent::__construct($message, \Plexo\Sdk\ResultCode::INVALID_SIGNATURE);
+    public function __construct($message, $previous = null) {
+        parent::__construct($message, \Plexo\Sdk\ResultCode::INVALID_SIGNATURE, $previous);
     }
 }
