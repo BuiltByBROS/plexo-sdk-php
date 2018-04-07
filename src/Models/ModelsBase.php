a9a66f772c57d33b0d28fd21671ccbf6d740d1b7
Pablo Bangueses <pablo.bangueses@bros.me>
2018-04-07 01:32:43 -0300

    Corrección en canonizado de números decimales y validación en PaymentRequest.

--- src/Models/ModelsBase.php
+++ src/Models/ModelsBase.php
@@ -1,6 +1,8 @@
 <?php
 namespace Plexo\Sdk\Models;
 
+use Plexo\Sdk\Exception;
+
 abstract class ModelsBase implements \ArrayAccess
 {
     /**
@@ -28,6 +30,33 @@ abstract class ModelsBase implements \ArrayAccess
 
     public function offsetGet($offset) {
         return isset($this{$offset}) ? $this->{$offset} : null;
+    }
+
+    public function validate()
+    {
+        $scheme = call_user_func(array(get_called_class(), 'getValidationMetadata'));
+        $errors = [];
+        foreach ($scheme as $key => $val) {
+            if ($val['required'] && is_null($this->data[$key])) {
+                array_push($errors, new Exception\InvalidArgumentException(sprintf('%s cannot be empty', $key)));
+            }
+            switch ($val['type']) {
+                case 'int':
+                    if (!is_int($this->data[$key]) && is_numeric($this->data[$key])) {
+                        $this->data[$key] = (int) $this->data[$key];
+                    }
+                    break;
+                case 'string':
+                    if (!is_string($this->data[$key]) && is_scalar($this->data[$key])) {
+                        $this->data[$key] = (string) $this->data[$key];
+                    }
+                    break;
+            }
+            if (!is_null($this->data[$key]) && $val['type'] !== 'class' && !call_user_func('is_' . $val['type'], $this->data[$key])) {
+                array_push($errors, new Exception\InvalidArgumentException(sprintf('%s must be of type %s, %s given.', $key, $val['type'], gettype($this->data[$key]))));
+            }
+        }
+        return count($errors) ? $errors : false;
     }
 
     public static function fromArray($data)
